73ff0190c654ba8445fe2783112f8d3f0db9c815
Prashanth Kumar <prashusiri.1990@gmail.com>
2025-02-07 11:42:52 +0530

    Mobile nav chnages

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,8 +1,16 @@
 import { NavLink, useNavigate } from 'react-router-dom';
+import { GiHamburgerMenu } from "react-icons/gi";
+import { useState } from 'react';
+import { IoCloseOutline } from "react-icons/io5";
 
 const Nav = () => {
 
     const navigate = useNavigate();
+    const [isOpen, setIsOpen] = useState(false);
+
+    const openNav = () => {
+        setIsOpen(!isOpen);
+    }
 
     return ( 
         <nav>
@@ -10,13 +18,27 @@ const Nav = () => {
                 <p className='logo'>EL</p>
                 <p className='logo-ab'>EasyLoan</p>
             </div>
-            <ul>
+            <ul className='nav'>
                 <li><NavLink to="">Home</NavLink></li>
                 <li><NavLink to="about">About</NavLink></li>
                 <li><NavLink to="services">Services</NavLink></li>
                 <li><NavLink to="Contact">Contact</NavLink></li>
                 <li><button className='nav-btn' onClick={() => navigate('apply-loan')}>Apply for loan</button></li>
             </ul>
+            <div className='mobile-nav'>
+                <GiHamburgerMenu onClick={() => openNav()} />
+                {isOpen && <div className='mobile-menu'>
+                    <div className='close-icon'><IoCloseOutline onClick={() => setIsOpen(false)}/></div>
+                    <ul>
+                        <li><NavLink to="" onClick={() => openNav()}>Home</NavLink></li>
+                        <li><NavLink to="about" onClick={() => openNav()}>About</NavLink></li>
+                        <li><NavLink to="services" onClick={() => openNav()}>Services</NavLink></li>
+                        <li><NavLink to="Contact" onClick={() => openNav()}>Contact</NavLink></li>
+                        <li><button className='nav-btn' onClick={() => navigate('apply-loan')}>Apply for loan</button></li>
+                    </ul> 
+                    </div> 
+                } 
+            </div>
         </nav>
      );
 }
